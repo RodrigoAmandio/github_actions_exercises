7c0efa54208a4da28042a9adbb3f8ab89e82676e
Rodrigo Amandio <rodrigo.amandio@deepesg.com>
2025-05-19 18:16:58 -0300

    feat: Reformata os arquivos com black e adiciona as dependências necessárias em requirements.txt

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -2,12 +2,13 @@ import unittest
 
 from python.main import soma
 
+
 class TestStringMethods(unittest.TestCase):
 
     def test_soma(self):
         x = 4
         y = 8
 
-        result = soma(x,y)
+        result = soma(x, y)
 
-        self.assertEqual(result,12)
+        self.assertEqual(result, 12)
